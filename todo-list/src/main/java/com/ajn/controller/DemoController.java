5e8fa1133b1135680fb7032fe29e6b1e1617267e
TheAjN <abhisheknaik6@gmail.com>
2021-10-31 00:15:20 +0530

    ViewResolver and Views

--- todo-list/src/main/java/com/ajn/controller/DemoController.java
+++ todo-list/src/main/java/com/ajn/controller/DemoController.java
@@ -8,11 +8,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class DemoController {
 
 	// http://localhost:8080/todo-list/hello
-	@GetMapping("/Hello")
+	@GetMapping("Hello")
 //	@RequestMapping(value = "/Hello", method = RequestMethod.GET)
 	@ResponseBody
 	public String hello() {
 		return "Hello";   // Return value should be same as that of inside @GetMapping(_)
+	}
+	
+	
+	// http://localhost:8080/todo-list/welcome
+	// prefix + name + suffix
+	// /WEB-INF/view/welcome.jsp
+	@GetMapping("welcome") // here "/" is not needed because it is mentioned in the .addMapping in WebAppInitializer.class (DispatcherServlet)
+	public String welcome() {
+		return "welcome"; // In this case,  welcome returned here is the name of the .jsp file,
+						//ie, "welcome" won't be displayed in the browser, but the contents of welcome.jsp
 	}
 	
 }
