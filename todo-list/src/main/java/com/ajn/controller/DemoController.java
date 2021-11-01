908f4690f8b2d793e80e23632303f06e64b31f0b
TheAjN <abhisheknaik6@gmail.com>
2021-11-01 22:52:03 +0530

    Model & Model Attributes

--- todo-list/src/main/java/com/ajn/controller/DemoController.java
+++ todo-list/src/main/java/com/ajn/controller/DemoController.java
@@ -1,11 +1,17 @@
 package com.ajn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class DemoController {
+	
 
 	// http://localhost:8080/todo-list/hello
 	@GetMapping("Hello")
@@ -17,12 +23,24 @@ public class DemoController {
 	
 	
 	// http://localhost:8080/todo-list/welcome
-	// prefix + name + suffix
-	// /WEB-INF/view/welcome.jsp
+	
 	@GetMapping("welcome") // here "/" is not needed because it is mentioned in the .addMapping in WebAppInitializer.class (DispatcherServlet)
-	public String welcome() {
+	public String welcome(Model model) {
+		
+		model.addAttribute("user","Abhishek J");  //"user" is key , "Abhishek J" is value. Here "user" will be mapped to the user in welcome.jsp
+		
+		// prefix + name + suffix
+		// /WEB-INF/view/welcome.jsp
 		return "welcome"; // In this case,  welcome returned here is the name of the .jsp file,
-						//ie, "welcome" won't be displayed in the browser, but the contents of welcome.jsp
+						//ie, "welcome" won't be displayed in the browser, but the contents of welcome.jsp will be displayed
+	}
+	
+	@ModelAttribute("welcomeMessage") //Another way of adding attributes to the model is by @ModelAttribute, 
+	public String welcomeMessage() {  //Where "welcomeMessage" acts as a key and the returned value is the value which will be displayed in the webpage
+		
+		log.info("welcomeMessage() called"); 
+		
+		return "Welcome to this Demo Application.";
 	}
 	
 }
--- todo-list/src/main/webapp/WEB-INF/view/welcome.jsp
+++ todo-list/src/main/webapp/WEB-INF/view/welcome.jsp
@@ -1,8 +1,11 @@
 <html>
 <head>
-<title>Welcome</title>
+<title>Welcome</title> <!-- This jsp file is will be called 
+					when the Request method welcome() returns "welcome", 
+					which is equivalent to this .jsp file's name-->
 </head>
 <body>
-	<h1>Hello world</h1>
+	<h1>Hello ${user}</h1>  <!-- ${user} is from model.addrtibute() where "Abhishek J" is the value  -->
+	<h2>${welcomeMessage}</h2> <!-- From the @ModelAttribute in the Controller -->>
 </body>
 </html>
